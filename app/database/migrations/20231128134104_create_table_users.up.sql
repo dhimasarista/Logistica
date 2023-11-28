c353fe6ed77c937dc03f487869480915588ec8ac
dhimasarista <dhimasharrisst@gmail.com>
2023-11-28 20:51:19 +0700

    Change Migration(users)

--- app/database/migrations/20231128134104_create_table_users.up.sql
+++ app/database/migrations/20231128134104_create_table_users.up.sql
@@ -2,5 +2,7 @@ CREATE TABLE users(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     username VARCHAR(255),
     password TEXT, 
+    employee_id INT,
+    FOREIGN KEY (employee_id) REFERENCES employees(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE = InnoDB;
