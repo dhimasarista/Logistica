fa72de196c7a9241fb230ca6a0c5466c8f5576e9
dhimasarista <mdhimasarista@gmail.com>
2023-12-05 05:30:16 +0700

    Fixed Employee Model

--- app/models/employee_model.go
+++ app/models/employee_model.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"log"
 	"logistica/app/config"
+	"logistica/app/utility"
 )
 
 type Employee struct {
@@ -12,7 +13,7 @@ type Employee struct {
 	Name        sql.NullString `json:"name"`
 	Address     sql.NullString `json:"address"`
 	NumberPhone sql.NullString `json:"number_phone"`
-	Position    sql.NullString `json:"position"`
+	Position    sql.NullInt64  `json:"position"`
 	IsUser      sql.NullBool   `json:"is_user"`
 	IsSuperuser sql.NullBool   `json:"is_superuser"`
 }
@@ -21,7 +22,7 @@ func (e *Employee) GetById(id int64) error {
 	var db = config.ConnectDB()
 	defer db.Close()
 
-	var query string = "SELECT id, name, address, number_phone, position, is_user, is_superuser FROM employees WHERE id = ?"
+	var query string = "SELECT id, name, address, number_phone, position_id, is_user, is_superuser FROM employees WHERE id = ?"
 	err := db.QueryRow(query, id).Scan(
 		&e.ID,
 		&e.Name,
@@ -41,7 +42,7 @@ func (e *Employee) FindAll() ([]map[string]any, error) {
 	var db = config.ConnectDB()
 	defer db.Close()
 
-	var query string = "SELECT id, name, address, number_phone, position, is_user, is_superuser FROM employees WHERE id > 1"
+	var query string = "SELECT e.id, e.name as employee_name, e.address, e.number_phone, e.position_id, e.is_user, e.is_superuser, p.name AS position_name FROM employees e JOIN positions p ON e.position_id = p.id WHERE e.id > 1"
 	ctx := context.Background()
 	rows, err := db.QueryContext(ctx, query)
 	if err != nil {
@@ -52,6 +53,7 @@ func (e *Employee) FindAll() ([]map[string]any, error) {
 	defer rows.Close()
 
 	var employees []map[string]any
+	var positionName sql.NullString
 
 	for rows.Next() {
 		err := rows.Scan(
@@ -62,6 +64,7 @@ func (e *Employee) FindAll() ([]map[string]any, error) {
 			&e.Position,
 			&e.IsUser,
 			&e.IsSuperuser,
+			&positionName,
 		)
 
 		if err != nil {
@@ -70,13 +73,13 @@ func (e *Employee) FindAll() ([]map[string]any, error) {
 		}
 
 		var employee = map[string]any{
-			"id":          e.ID.Int64,
-			"name":        e.Name.String,
-			"address":     e.Address.String,
-			"position":    e.Position.String,
-			"numberPhone": e.NumberPhone.String,
-			"isUser":      e.IsUser.Bool,
-			"isSuperuser": e.IsSuperuser.Bool,
+			"id":           e.ID.Int64,
+			"name":         utility.Capitalize(e.Name.String),
+			"address":      utility.Capitalize(e.Address.String),
+			"positionName": utility.Capitalize(positionName.String),
+			"numberPhone":  utility.Capitalize(e.NumberPhone.String),
+			"isUser":       e.IsUser.Bool,
+			"isSuperuser":  e.IsSuperuser.Bool,
 		}
 
 		employees = append(employees, employee)
