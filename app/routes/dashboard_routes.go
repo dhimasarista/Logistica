3251031fdea8e3e3ab5b6c3143e5e3e87914565a
dhimasarista <dhimasharrisst@gmail.com>
2023-12-05 17:55:17 +0700

    Add TotalEmployees

--- app/routes/dashboard_routes.go
+++ app/routes/dashboard_routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"logistica/app/controllers"
+	"logistica/app/models"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
@@ -11,10 +12,17 @@ func DashboardRoutes(app *fiber.App, store *session.Store) {
 	app.Get("/dashboard", func(c *fiber.Ctx) error {
 		var path string = c.Path()
 		var username string = controllers.GetSessionUsername(c, store)
+		employees := models.Employee{}
+
+		totalEmployees, err := employees.Count()
+		if err != nil {
+			return InternalServerError(c, err.Error())
+		}
 		// Mengirimkan halaman HTML yang dihasilkan ke browser
 		return c.Render("dashboard", fiber.Map{
-			"path": path,
-			"user": username,
+			"path":           path,
+			"user":           username,
+			"totalEmployees": totalEmployees,
 		})
 	},
 	)
