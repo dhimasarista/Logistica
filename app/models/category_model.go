b2f32efd95e6a1efadab86c0bccceeb346c6d319
dhimasarista <dhimasharrisst@gmail.com>
2023-12-18 13:23:33 +0700

    Add NewCategory Handling

--- app/models/category_model.go
+++ app/models/category_model.go
@@ -3,8 +3,11 @@ package models
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"logistica/app/config"
 	"logistica/app/utility"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 type Category struct {
@@ -43,6 +46,31 @@ func (c *Category) FindAll() ([]map[string]interface{}, error) {
 	}
 
 	return categories, nil
+}
+
+func (c *Category) NewCategory(id int, name string) (sql.Result, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	var db = config.ConnectDB()
+	defer db.Close()
+
+	// Jika id yang diterima di bawah 9100
+	if id <= 889 {
+		id = 890 // sebagai nilai set otomatis jika row belum ada
+	}
+	var query string = "INSERT INTO product_category VALUES(?, ?)"
+	result, err := db.Exec(query, id, name)
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			if mysqlErr.Number == 1062 {
+				return nil, errors.New("race condition, id has been taken")
+			}
+		}
+		return result, err
+	}
+
+	return result, nil
 }
 func (c *Category) LastId() (int, error) {
 	mutex.Lock()
--- app/routes/inventory_routes.go
+++ app/routes/inventory_routes.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"logistica/app/controllers"
 	"logistica/app/models"
@@ -16,9 +15,10 @@ import (
 func InventoryRoutes(app *fiber.App, store *session.Store) {
 	var productModel *models.Product = &models.Product{}
 	var manufacturerModel *models.Manufacturer = &models.Manufacturer{}
-	category := &models.Category{}
+	categoryModel := &models.Category{}
 
 	app.Post("/product/new", func(c *fiber.Ctx) error {
+		// Mendapatkan ID terakhir dari produk
 		lastId, err := productModel.LastId()
 		if err != nil {
 			log.Println(err)
@@ -28,7 +28,7 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			})
 		}
 
-		var formData map[string]string // variabel untuk menyimpan data yang diterima dari client-side
+		var formData map[string]string // Variabel untuk menyimpan data yang diterima dari client-side
 		body := c.Body()
 		err = json.Unmarshal(body, &formData)
 		if err != nil {
@@ -37,12 +37,15 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 				"error": err.Error(),
 			})
 		}
-		if string(formData["manufacturer"]) == "" || string(formData["name"]) == "" {
+
+		// Memeriksa apakah data penting seperti 'manufacturer' dan 'name' kosong
+		if string(formData["manufacturer"]) == "" || string(formData["name"]) == "" || string(formData["category"]) == "" {
 			return c.JSON(fiber.Map{
 				"error":  "Form is Empty",
 				"status": fiber.StatusBadRequest,
 			})
 		}
+
 		// Memeriksa data manufacturer yang diterima
 		var manufacturerData int
 		// Jika dikirim dalam bentuk string number
@@ -51,9 +54,8 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			// Kemudian data dikonversi dari str ke integer
 			manufacturerStrToInt, _ := strconv.Atoi(formData["manufacturer"])
 			manufacturerData = manufacturerStrToInt
-			// Jika yang dikirim adalah string char
 		} else {
-			// Maka dibuat row data manufacturer baru
+			// Jika yang dikirim adalah string char, maka dibuat row data manufacturer baru
 			// Dengan patokan id terakhir
 			lastIdManufacturer, _ := manufacturerModel.LastId()
 			var newIdManufacturer = lastIdManufacturer + 1
@@ -67,11 +69,29 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			manufacturerData = newIdManufacturer
 		}
 
+		var categoryData int
+		if utility.IsNumeric(formData["category"]) {
+			categoryStrToInt, _ := strconv.Atoi(formData["category"])
+			categoryData = categoryStrToInt
+		} else {
+			lastIdCategory, _ := categoryModel.LastId()
+			var newIdCategory = lastIdCategory + 1
+			_, err := categoryModel.NewCategory(newIdCategory, formData["category"])
+			if err != nil {
+				log.Println(err)
+				return c.JSON(fiber.Map{
+					"error": err.Error(),
+				})
+			}
+			categoryData = newIdCategory
+		}
+
+		// Mengkonversi string ke integer untuk beberapa atribut
 		stocksStrToInt, _ := strconv.Atoi(formData["stocks"])
 		priceStrToInt, _ := strconv.Atoi(formData["price"])
 		weightStrToint, _ := strconv.Atoi(formData["weight"])
-		categoryStrToInt, _ := strconv.Atoi(formData["category"])
 
+		// Menyusun hasil data produk
 		results := map[string]interface{}{
 			"id":            lastId + 1,
 			"name":          string(formData["name"]),
@@ -80,41 +100,47 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			"stocks":        stocksStrToInt,
 			"price":         priceStrToInt,
 			"weight":        weightStrToint,
-			"category":      categoryStrToInt,
+			"category":      categoryData,
 		}
-		fmt.Println("After", formData)
-		// fmt.Println("After", results)
 
+		// Mengembalikan respons JSON setelah menambahkan produk baru
 		return c.JSON(fiber.Map{
 			"error":   nil,
 			"status":  fiber.StatusOK,
-			"message": "Succes Add New Product",
+			"message": "Success Add New Product",
 			"result":  results,
 		})
 	})
 
 	app.Get("/inventory", func(c *fiber.Ctx) error {
+		// Mendapatkan path dari URL
 		var path string = c.Path()
+
+		// Mendapatkan nama pengguna dari sesi
 		var username string = controllers.GetSessionUsername(c, store)
 
+		// Mengambil semua produk dari model
 		products, err := productModel.FindAll()
 		if err != nil {
 			log.Println(err)
 			InternalServerError(c, err.Error())
 		}
 
+		// Mengambil semua manufaktur dari model
 		manufacturers, err := manufacturerModel.FindAll()
 		if err != nil {
 			log.Println(err)
 			InternalServerError(c, err.Error())
 		}
 
-		categories, err := category.FindAll()
+		// Mengambil semua kategori dari model
+		categories, err := categoryModel.FindAll()
 		if err != nil {
 			log.Println(err)
 			InternalServerError(c, err.Error())
 		}
 
+		// Merender halaman "inventory_page" dengan data yang diperlukan
 		return c.Render("inventory_page", fiber.Map{
 			"path":          path,
 			"user":          username,
@@ -123,12 +149,12 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			"categories":    categories,
 		})
 	})
-
-	// app.Post("/inventory/")
-
+	// Endpoint untuk mendapatkan detail produk berdasarkan ID
 	app.Get("/inventory/product/:id", func(c *fiber.Ctx) error {
+		// Mendapatkan ID produk dari parameter URL
 		productID := c.Params("id")
 
+		// Mengonversi ID produk ke dalam bentuk integer
 		productIDInteger, err := strconv.Atoi(productID)
 		if err != nil {
 			return c.JSON(fiber.Map{
@@ -137,6 +163,7 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			})
 		}
 
+		// Mengambil produk dari model berdasarkan ID
 		err = productModel.GetById(productIDInteger)
 		if err != nil {
 			return c.JSON(fiber.Map{
@@ -145,6 +172,7 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			})
 		}
 
+		// Menyiapkan data untuk respons JSON
 		data := map[string]interface{}{
 			"id":             productModel.ID.Int64,
 			"name":           productModel.Name.String,
@@ -158,24 +186,32 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			"weight":         productModel.Weight.Int64,
 		}
 
+		// Mengembalikan respons JSON dengan data produk
 		return c.JSON(fiber.Map{
 			"data": data,
 		})
 	})
 
+	// Endpoint untuk memeriksa keberadaan ID dalam database
 	app.Get("/inventory/check/:id", func(c *fiber.Ctx) error {
+		// Mendapatkan ID dari parameter URL
 		idString := c.Params("id")
 		idInteger, err := strconv.Atoi(idString)
 		var isIdExists bool = false
+
+		// Menangani kesalahan jika konversi ID ke integer tidak berhasil
 		if err != nil {
 			log.Println(err)
 		}
+
+		// Jika ID adalah 0, mengindikasikan produk baru
 		if idInteger == 0 {
 			return c.JSON(fiber.Map{
 				"new_product": true,
 			})
 		}
 
+		// Memeriksa keberadaan ID dalam database
 		dataId, err := productModel.OnlyGetID(idInteger)
 		if err != nil {
 			log.Println(err)
@@ -184,18 +220,25 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 				"status": 500,
 			})
 		}
+
+		// Menetapkan isIdExists menjadi true jika ID ditemukan dalam database
 		if int64(dataId) == int64(idInteger) {
 			isIdExists = true
 		}
 
+		// Mengembalikan respons JSON dengan hasil pemeriksaan ID
 		return c.JSON(fiber.Map{
 			"is_id_exists":    isIdExists,
 			"response_status": c.Response().StatusCode(),
 		})
 	})
 
+	// Endpoint untuk memperbarui stok produk berdasarkan ID
 	app.Post("/inventory/stocks/update", func(c *fiber.Ctx) error {
-		var formData map[string]any
+		// Mendeklarasikan variabel untuk menyimpan data yang diterima dari client-side
+		var formData map[string]interface{}
+
+		// Memparsing data dari body request menjadi bentuk map
 		err := c.BodyParser(&formData)
 		if err != nil {
 			log.Println("Body Parser", err)
@@ -205,8 +248,11 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			})
 		}
 
+		// Mengambil ID produk dari formData dan mengonversinya ke dalam tipe data integer
 		idStr := formData["id"].(string)
 		id, _ := strconv.Atoi(idStr)
+
+		// Mendapatkan stok terakhir dari produk
 		lastStocks, err := productModel.LastStocks(id)
 		if err != nil {
 			log.Println(err)
@@ -216,8 +262,11 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			})
 		}
 
+		// Mengambil jumlah stok yang akan ditambahkan dari formData dan mengonversinya ke dalam tipe data integer
 		stockStr := formData["amountStocks"].(string)
 		stock, _ := strconv.Atoi(stockStr)
+
+		// Memperbarui stok produk
 		results, err := productModel.UpdateStocks(id, lastStocks+stock)
 		if err != nil {
 			log.Println(err)
@@ -227,6 +276,7 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 			})
 		}
 
+		// Mengembalikan respons JSON dengan pesan sukses dan hasil pembaruan stok
 		return c.JSON(fiber.Map{
 			"message": "Stock Updated!",
 			"results": results,
@@ -234,3 +284,11 @@ func InventoryRoutes(app *fiber.App, store *session.Store) {
 		})
 	})
 }
+
+/*
+	Server tidak dapat menerima atau merespons data kecuali jika data tersebut
+	berbentuk formData["data"].(string) atau interface{}.
+	- Jika formData["data"].(int), tidak akan ada respons error.
+	- Data yang diterima dari klien dipaksa menjadi string sebelum dikonversi
+	  sesuai dengan tipe data yang akan diterima oleh DBMS.
+*/
